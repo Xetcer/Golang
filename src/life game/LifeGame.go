04413f4ab9f51d9ba439e14bf8343d8f4bfba88e
Xetcer <Ferdinand_tema@mail.ru>
2020-06-16 19:44:31 +0500

    game finished

--- src/life game/LifeGame.go
+++ src/life game/LifeGame.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const (
@@ -36,8 +38,57 @@ func (u Universe) Show() {
 	}
 }
 
+// Seed во вселенной живые клетки - 25% от ее вместимости
+func (u Universe) Seed() {
+	for i := 0; i < (width*height)/4; i++ {
+		u[rand.Intn(height)][rand.Intn(width)] = true
+	}
+}
+
+//Alive возвращает жива ли клета по координатам
+func (u Universe) Alive(x, y int) bool {
+	x = (x + width) % width
+	y = (y + height) % height
+	return u[y][x]
+}
+
+//Neighbors возвращает число соседей у данной клетки
+func (u Universe) Neighbors(x, y int) int {
+	n := 0
+	for v := -1; v <= 1; v++ {
+		for h := -1; h <= 1; h++ {
+			if !(v == 0 && h == 0) && u.Alive(x+h, y+v) {
+				n++
+			}
+		}
+	}
+	return n
+}
+
+// Next состояние вселенной на следующем шаге
+func (u Universe) Next(x, y int) bool {
+	n := u.Neighbors(x, y)
+	return n == 3 || n == 2 && u.Alive(x, y)
+}
+
+// Step определяет состояние новой вселенной по старой
+func Step(a, b Universe) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			b[y][x] = a.Next(x, y)
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Здесь скоро будет игра имитирующая жизнь клеток")
-	myUniverse := NewUniverse()
-	myUniverse.Show()
+	a, b := NewUniverse(), NewUniverse()
+	a.Seed()
+	for i := 0; i < 10; i++ {
+		Step(a, b)
+		fmt.Print("\x0c") // очищаем экран
+		a.Show()
+		time.Sleep(time.Second / 30)
+		a, b = b, a
+	}
 }
