d458e00eca811916ed194ff3c98400ca8d743808
Xetcer <ferdinand_tema@mail.ru>
2020-07-25 19:26:11 +0500

    Мелкие правки листин 3-4

--- src/Lesson 27/Lesson 27.go
+++ src/Lesson 27/Lesson 27.go
@@ -3,8 +3,8 @@ package main
 
 import "fmt"
 
-// List1 листинг 1
-func List1() {
+// List12 листинг 1-2
+func List12() {
 	fmt.Println("Листинг 1 указатели и адрес в памяти")
 	variable := 42
 	fmt.Println("Адрес в памяти переменной", &variable)
@@ -13,7 +13,21 @@ func List1() {
 	fmt.Println(*(&variable))
 }
 
+// List34 листинг 3-4
+func List34() {
+	fmt.Println("Листинг 3-4")
+	answer := 42
+	address := &answer
+	fmt.Printf("Тип %T\n", address)
+	canada := "Canada"
+	var home *string
+	fmt.Printf("home is a %T\n", home)
+	home = &canada
+	fmt.Println(*home)
+}
+
 func main() {
 	fmt.Println("Урок 27 указатели в Go")
-	List1()
+	List12()
+	List34()
 }
