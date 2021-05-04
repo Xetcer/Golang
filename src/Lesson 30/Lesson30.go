9688d517036eb5f518244755a7333abf71b846e3
Xetcer <Ferdinand_tema@mail.ru>
2021-05-04 20:40:31 +0500

    Листинг 2-3 создание нескольких горутин

--- src/Lesson 30/Lesson30.go
+++ src/Lesson 30/Lesson30.go
@@ -13,11 +13,27 @@ import (
 // listOne листинг 1 запуск горутины
 func listOneGopher() {
 	time.Sleep(3 * time.Second) // гофер спит
-	fmt.Println("... snore ...")
+	fmt.Println("гофер листинг 1 закончил работу")
+}
+
+// litTwoGopher запуск нескольких горутин
+func litTwoGopher() {
+	fmt.Println("Листинг 2 - 3 запуск 5 гоферов с параметрами") // в результате видим что гофера работают не по порядку
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i + 1)
+	}
+}
+
+// sleepyGopher тело одного гофера для листинга 2
+func sleepyGopher(number int) {
+	time.Sleep(3 * time.Second)
+	fmt.Printf("... гофер ... %d", number)
 }
 
 func main() {
 	fmt.Println("Урок 30, горутины в Go")
-	go listOneGopher()          // начало горутины
+	fmt.Println("Листинг 1 создание гофера")
+	go listOneGopher() // начало горутины\
+	litTwoGopher()
 	time.Sleep(4 * time.Second) // Ожидаем когда гофер дас о себе знать.
 }
