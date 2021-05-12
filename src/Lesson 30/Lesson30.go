6125809e0e9148ac33b6dff4ae8a06c1494333d9
Xetcer <Ferdinand_tema@mail.ru>
2021-05-12 17:10:21 +0500

    каналы связи между горутинами

--- src/Lesson 30/Lesson30.go
+++ src/Lesson 30/Lesson30.go
@@ -27,7 +27,14 @@ func litTwoGopher() {
 // sleepyGopher тело одного гофера для листинга 2
 func sleepyGopher(number int) {
 	time.Sleep(3 * time.Second)
-	fmt.Printf("... гофер ... %d", number)
+	fmt.Printf("... гофер ... %d\r\n", number)
+}
+
+// list4Gopher гофер с каналом связи
+func list4Gopher(id int, c chan int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("...", id, "snore ...")
+	c <- id // отправляем значение обратно в Main
 }
 
 func main() {
@@ -36,4 +43,13 @@ func main() {
 	go listOneGopher() // начало горутины\
 	litTwoGopher()
 	time.Sleep(4 * time.Second) // Ожидаем когда гофер дас о себе знать.
+	fmt.Println("Листинг 4 каналы связи между гоферами")
+	c := make(chan int) // Создаем канал связи между гоферами.
+	for i := 0; i < 5; i++ {
+		go list4Gopher(i, c)
+	}
+	for i := 0; i < 5; i++ {
+		gopherID := <-c // получаем значение из канала
+		fmt.Println("gopher ", gopherID, " has finished sleeping")
+	}
 }
